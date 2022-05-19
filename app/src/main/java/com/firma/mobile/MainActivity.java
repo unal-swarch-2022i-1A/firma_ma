5d924a1c5d7efe662bb5d1fd0a68f22c1a0bbd82
FredyRosero <faroseroc@unal.edu.co>
2022-05-19 03:32:37 -0500

    Folders

--- app/src/main/java/com/firma/mobile/MainActivity.java
+++ app/src/main/java/com/firma/mobile/MainActivity.java
@@ -1,14 +1,27 @@
 package com.firma.mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.firma.mobile.app_name";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, SigningActivity.class);
+        String message = "message";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
