c23cc640aec6aca387db0abd089cc10b7e8eec7c
FredyRosero <faroseroc@unal.edu.co>
2022-05-19 07:43:54 -0500

    Side nav Mat intent

--- app/src/main/java/com/firma/mobile/MainActivity.java
+++ app/src/main/java/com/firma/mobile/MainActivity.java
@@ -1,27 +1,50 @@
 package com.firma.mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.firma.mobile.app_name";
-
+    private Toolbar topAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, SigningActivity.class);
-        String message = "message";
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+
+        DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation);
+
+        getSupportActionBar().hide();//Ocultar ActivityBar anterior
+        topAppBar = (Toolbar)findViewById(R.id.topAppBar);
+        //setSupportActionBar(topAppBar);
+
+//        topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("TOU AQUI", "TOU AQUI");
+//            }
+//        });
+            //drawerLayout.open()
+
+/*
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+                // Handle menu item selected
+                menuItem.isChecked = true
+            drawerLayout.close()
+            true
+        }*/
     }
 
 }
